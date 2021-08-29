b046ae043859acfb027f0be820e64fec4f47046b
Miloslav Staněk <milostanek@gmail.com>
2021-08-29 18:45:32 +0200

    Add bassic structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='objconf',
+    version='0.1.0',
+    description='Object configuration for Python projects',
+    url='https://github.com/milosta/objconf',
+
+    author='Miloslav Stanek',
+    author_email='milostanek@gmail.com',
+
+    packages=find_packages(exclude=['docs', 'tests']),
+    python_requires='>=3.6',
+    install_requires=[
+        # TODO
+    ],
+)
