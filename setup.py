cec095450ffc49ad91abec2487441057a9b79ed8
Miloslav Staněk <milostanek@gmail.com>
2021-09-22 09:44:42 +0200

    Toxify tests

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 
+test_dependencies = [
+    'coverage',
+    'pytest',
+]
+extras = {
+    'tests': test_dependencies,
+}
+
 setup(
     name='objconf',
     version='0.2.0',
     description='Object configuration for Python projects',
+    long_description=open('README.md').read(),
     url='https://github.com/milosta/objconf',
 
     author='Miloslav Stanek',
@@ -14,7 +23,6 @@ setup(
     install_requires=[
         'pyaml',
     ],
-    tests_require=[
-        'pytest',
-    ],
+    tests_require=test_dependencies,
+    extras_require=extras,
 )
